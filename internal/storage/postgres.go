310c8e181cc67a54eb8503c4535ee971d4bd92f9
krishnaGauss <goswamikrishna549@gmail.com>
2025-07-03 00:33:34 +0530

    completed with storage query methods v1.0

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -1,9 +1,12 @@
 package storage
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"log/slog"
+	"strings"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/krishnaGauss/SoCode/internal/config"
@@ -86,18 +89,104 @@ func (s *PostgresStorage) StoreLogs(logs []models.LogEntry) error {
 	return err
 }
 
-func (s *PostgresStorage) QueryLogs(query models.LogQuery) ([]models.LogEntry, error){
+func (s *PostgresStorage) QueryLogs(query models.LogQuery) ([]models.LogEntry, error) {
 	var conditions []string
 	var args []interface{}
-	argCount:=0
+	argCount := 0
 
-	baseQuery:="SELECT id, timestamp, level, message, source, service, host, tags, metadata FROM logs"
+	baseQuery := "SELECT id, timestamp, level, message, source, service, host, tags, metadata FROM logs"
 
-	if query.StartTime!=nil{
+	if query.StartTime != nil {
 		argCount++
 		conditions = append(conditions, fmt.Sprintf("timestamp >= $%d", argCount))
 		args = append(args, *query.StartTime)
 	}
 
-	return nil, nil //to be completed
+	if query.EndTime != nil {
+		argCount++
+		conditions = append(conditions, fmt.Sprintf("timestamp <= $%d", argCount))
+		args = append(args, *query.EndTime)
+	}
+
+	if len(query.Level) > 0 {
+		argCount++
+		conditions = append(conditions, fmt.Sprintf("level = ANY($%d)", argCount))
+		levels := make([]string, len(query.Level))
+		for i, level := range query.Level {
+			levels[i] = string(level)
+		}
+
+		args = append(args, levels)
+	}
+
+	if len(query.Source) > 0 {
+		argCount++
+		conditions = append(conditions, fmt.Sprintf("source = ANY($%d)", argCount))
+		args = append(args, query.Source)
+	}
+
+	if query.Search != "" {
+		argCount++
+		conditions = append(conditions, fmt.Sprintf("message ILIKE $%d", argCount))
+		args = append(args, "%"+query.Search+"%")
+	}
+
+	if len(conditions) > 0 {
+		baseQuery += "WHERE" + strings.Join(conditions, " AND ")
+	}
+
+	baseQuery += "ORDER BY timestamp DESC" //remember to put ;
+
+	if query.Limit > 0 {
+		argCount++
+		baseQuery += fmt.Sprintf(" LIMIT $%d", argCount)
+		args = append(args, query.Limit)
+	}
+
+	if query.Offset > 0 {
+		argCount++
+		baseQuery += fmt.Sprintf(" OFFSET $%d", argCount)
+		args = append(args, query.Offset)
+	}
+
+	rows, err := s.db.Query(baseQuery, args...) //using ... to help in using argument as an interface slice individually
+
+	if err != nil {
+		slog.Debug("cannot execute query in postgres")
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var logs []models.LogEntry
+	for rows.Next() {
+		var log models.LogEntry
+		var tagsJSON, metadataJSON sql.NullString
+
+		err := rows.Scan(
+			&log.ID, &log.Timestamp, &log.Level, &log.Message,
+			&log.Source, &log.Service, &log.Host, &tagsJSON, &metadataJSON,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		 if tagsJSON.Valid {
+            json.Unmarshal([]byte(tagsJSON.String), &log.Tags)
+        }
+        if metadataJSON.Valid {
+            log.Metadata = json.RawMessage(metadataJSON.String)
+        }
+
+        logs = append(logs, log)
+	}
+
+	return logs, nil
+
+}
+
+
+func (s *PostgresStorage) Close() error{
+	return s.db.Close()
 }
