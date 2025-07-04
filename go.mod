5d5556dafc73c0dd5f6f90fec3224f862b860100
krishnaGauss <goswamikrishna549@gmail.com>
2025-07-04 22:30:10 +0530

    Added grpc implementation file

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/krishnaGauss/SoCode
 go 1.23.5
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/lib/pq v1.10.9
 	github.com/redis/go-redis/v9 v9.11.0
--- internal/server/grpc_server.go
+++ internal/server/grpc_server.go
@@ -4,10 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/krishnaGauss/SoCode/internal/models"
 	"github.com/krishnaGauss/SoCode/internal/storage"
 	"github.com/krishnaGauss/SoCode/proto/SoCode/proto"
+
+	"github.com/google/uuid"
+	// "google.golang.org/grpc"
+    "google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type LogServer struct {
@@ -63,6 +68,87 @@ func (s *LogServer) SendLogStream(stream proto.LogService_SendLogStreamServer) e
     })
 }
 
-func (s *LogServer) protoToModel(req *proto.LogRequest) models.LogEntry {
+func (s *LogServer) QueryLogs(ctx context.Context, req *proto.QueryRequest) (*proto.QueryResponse, error){
+	query:=models.LogQuery{
+		Search: req.Search,
+        Limit:  int(req.Limit),
+        Offset: int(req.Offset),
+	}
 
+	if req.StartTime!=nil{
+		startTime:=req.StartTime.AsTime()
+		query.StartTime=&startTime
+	}
+
+	if req.EndTime != nil {
+        endTime := req.EndTime.AsTime()
+        query.EndTime = &endTime
+    }
+
+	for _, level:=range req.Levels{
+		query.Level = append(query.Level, models.LogLevel(level))
+	}
+
+	 query.Source = req.Sources
+    query.Service = req.Services
+    query.Host = req.Hosts
+    query.Tags = req.Tags
+
+	 logs, err := s.storage.QueryLogs(query)
+    if err != nil {
+        return nil, err
+    }
+
+    response := &proto.QueryResponse{
+        Total: int32(len(logs)),
+	}
+
+	 for _, log := range logs {
+        response.Logs = append(response.Logs, s.modelToProto(log))
+    }
+
+    return response, nil
 }
+
+func (s *LogServer) protoToModel(req *proto.LogRequest) models.LogEntry {
+	log:= models.LogEntry{
+		ID:      req.Id,
+        Level:   models.LogLevel(req.Level),
+        Message: req.Message,
+        Source:  req.Source,
+        Service: req.Service,
+        Host:    req.Host,
+        Tags:    req.Tags,
+	}
+
+	if req.Id==""{
+		log.ID=uuid.New().String()
+	}
+
+	 if req.Timestamp != nil {
+        log.Timestamp = req.Timestamp.AsTime()
+    } else {
+        log.Timestamp = time.Now()
+    }
+
+    if req.Metadata != "" {
+        log.Metadata = []byte(req.Metadata)
+    }
+
+    return log
+}
+
+func (s *LogServer) modelToProto(log models.LogEntry) *proto.LogRequest {
+    return &proto.LogRequest{
+        Id:        log.ID,
+        Timestamp: timestamppb.New(log.Timestamp),
+        Level:     string(log.Level),
+        Message:   log.Message,
+        Source:    log.Source,
+        Service:   log.Service,
+        Host:      log.Host,
+        Tags:      log.Tags,
+        Metadata:  string(log.Metadata),
+    }
+}
+
